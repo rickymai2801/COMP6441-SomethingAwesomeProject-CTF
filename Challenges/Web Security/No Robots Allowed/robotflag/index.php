d9e709b685ec3af52ca2d9aed970cf775356039f
rickymai2801 <147476925+rickymai2801@users.noreply.github.com>
2023-11-03 15:05:44 +1100

    Create index.php

---
+++ Challenges/Web Security/No Robots Allowed/robotflag/index.php
@@ -0,0 +1,25 @@
+<?php
+
+$content = 'flagisr0b0t51nd15gu153';
+
+echo <<<HTML
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+      <title>Websecurity1</title>
+      <meta charset="UTF-8">
+      <link rel="stylesheet" href="web1.css"/>
+  </head>
+  <body>
+  <div class="container">
+      <header>
+          <h1>Ricky's Website &#128526</h1>
+      </header>
+      <div id="intro" class="content" style="display: block;" >
+          $content
+      </div>
+  </div>
+  </body>
+  </html>
+  HTML;
+?>
